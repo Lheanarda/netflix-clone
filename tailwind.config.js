b1f655a21b53da77b77107f85dbb66eb77b6992d
lheanarda <alexanderleo0499@gmail.com>
2022-07-12 00:20:52 +0700

    home design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+       textShadow: {
+        md: '2px 2px 4px rgb(0 0 0 / 45%);',
+      },
+      backgroundImage: {
+        'gradient-to-b':
+          'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,0) 15%,rgba(20,20,20,.50) 50%,rgba(20,20,20,.80) 80%,#141414 95%,#141414 100%);',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow'), 
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar'),
+  ],
 }
